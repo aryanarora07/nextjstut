ae14f2cc5553ca091ccb3bf6a3e7fb2aefa0bf0a
aryanarora07 <aroraaryan824@gmail.com>
2023-09-24 18:51:03 -0700

    added starter code

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div>
+            <h1 className="text-center text-white text-6xl my-4">Login Page</h1>
+      </div>
+    );
+}
---
+++ src/app/singup/page.tsx
@@ -0,0 +1,10 @@
+export default function SignupPage()
+{
+    return (
+        <div>
+            <h1 className="text-center text-white text-6xl my-4">
+                SIGNUP PAGE
+            </h1>
+        </div>
+    );
+}
